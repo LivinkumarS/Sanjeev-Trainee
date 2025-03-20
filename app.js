e4e9a03bcea84d17614f57a6d03420cc88e0d796
LivinkumarS <001liv100@ggmail.com>
2025-03-20 18:25:59 +0530

    3 dtypes and 3 operaions

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
-const a = "Hello";
+// Number
 
-console.log(typeof true);
+// let num1 = 34.5387858583;
+
+// console.log(num1.toFixed(4)); //34.5387
+
+// console.log(num1);
+
+// let numStr = "638552.6238434";
+// console.log(typeof parseInt(numStr));
+
+// console.log(typeof parseFloat(numStr));
+
+// String
+
+// let str = "GSUGWVFWUFVJWEEFG";
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(Boolean(""));
+
+// Arithmetic Operations
+
+// let a = 23;
+// let b = 23;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b); //reminder
+
+// a++
+// a++
+// a--
+// a--
+// console.log(a);
+
+// Assignment Operator
+
+// let a = 47;
+
+// a += 12;
+// a -= 10;
+
+// a %= 2;
+
+// console.log(a);
+
+// Comparison Operators
+
+let a = 9;
+let b = 10;
+
+console.log(a >= b); 
