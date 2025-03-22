235227af07a8ef08cfa15ec71c255cfcba0325ba
LivinkumarS <001liv100@ggmail.com>
2025-03-22 18:11:12 +0530

    Logical Operations

--- app.js
+++ app.js
@@ -1,55 +1,17 @@
-// Number
+// Logical Operators
 
-// let num1 = 34.5387858583;
+let val1 = true;
+let val2 = false;
 
-// console.log(num1.toFixed(4)); //34.5387
+// console.log(val1 && val2);
+// console.log(val1 || val2);
+// console.log(val1 ^ val2);
+// console.log(!val2);
 
-// console.log(num1);
+// console.log(!val1 || !(val1 ^ val2));
 
-// let numStr = "638552.6238434";
-// console.log(typeof parseInt(numStr));
+console.log(
+  !val1 && ((val1 && val2) || (!val1 && val2 ^ val1) || !(val1 || val2))
+);
 
-// console.log(typeof parseFloat(numStr));
-
-// String
-
-// let str = "GSUGWVFWUFVJWEEFG";
-// console.log(str.length);
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-
-// console.log(Boolean(""));
-
-// Arithmetic Operations
-
-// let a = 23;
-// let b = 23;
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(a % b); //reminder
-
-// a++
-// a++
-// a--
-// a--
-// console.log(a);
-
-// Assignment Operator
-
-// let a = 47;
-
-// a += 12;
-// a -= 10;
-
-// a %= 2;
-
-// console.log(a);
-
-// Comparison Operators
-
-let a = 9;
-let b = 10;
-
-console.log(a >= b); 
+// BODMAS---> Brackets, Of, Division, Multip..,Add, Sub
