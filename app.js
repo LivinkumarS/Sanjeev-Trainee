01780ff4ca8a2c7268e2ae90e59efe1d67b3fd73
LivinkumarS <001liv100@ggmail.com>
2025-03-28 18:02:41 +0530

    ternary operator

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
-// Map
+// 10 < 6 ? console.log("vijay") : console.log("ajith");
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //+2
+let ans = (10 == "10") ? "Hi" : "Hello";
 
-// [3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-// [3,4,5,6,7,8,9,10,11,12];
-
-// let ans = arr.map((ele, index) => {
-//   return ele + 2;
-// });
-
-// console.log(ans);
-
-// Filter
-
-// [];
-
-// let ans = arr.filter((ele, ind) => {
-//   return ele > 5 && ele < 8;
-// });
-
-// console.log(ans);
-
-// Reduce
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// let ans = arr.reduce((acc, ele, ind) => {
-//   console.log(acc);
-//   return acc * ele;
-// }, 1);
-
-// console.log("Final: ", ans);
+console.log(ans);
