1a2eade31d0fe2ea9c923fd824b88695d40a23f7
LivinkumarS <001liv100@ggmail.com>
2025-04-15 18:02:08 +0530

    Map, Filter deep

--- app.js
+++ app.js
@@ -1,5 +1,68 @@
-// 10 < 6 ? console.log("vijay") : console.log("ajith");
+// object
 
-let ans = (10 == "10") ? "Hi" : "Hello";
+// let obj = {
+//   name: "sanjeev",
+//   role: "dev",
+//   favFoods: [
+//     "poori",
+//     "chapati",
+//     "idly",
+//     { mostFav: "biriyani", leastFav: "upma" },
+//   ],
+//   printName: () => {
+//     console.log("Name is sanjeev");
+//   },
+// };
 
-console.log(ans);
+// console.log(obj.favFoods[3].mostFav);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// [3,4,5,6,7,8,9,10,11,12] (+2)
+
+// [0,2,6]
+
+// let ans = arr.map((ele, ind) => {
+//   return ele*ind;
+// });
+
+// console.log(ans);
+
+// Filter
+
+// let arr = [1, 2, 3, 4, 5, 56, 7, 8, 9, 7, 6, 54, 35, 4, 46];
+// ele>10
+
+// [1,2,3...]
+
+// let ans = arr.filter((ele, ind) => {
+//   return ele>10;
+// });
+
+// console.log(ans);
+
+// above 18
+
+// let arr = [
+//   { age: 27, canVote: false },
+//   { age: 2, canVote: false },
+//   { age: 7, canVote: false },
+//   { age: 37, canVote: false },
+//   { age: 7, canVote: false },
+//   { age: 71, canVote: false },
+//   { age: 86, canVote: false },
+//   { age: 2, canVote: false },
+//   { age: 25, canVote: false },
+//   { age: 1, canVote: false },
+//   { age: 7, canVote: false },
+//   { age: 127, canVote: false },
+//   { age: 67, canVote: false },
+//   { age: 97, canVote: false },
+// ];
+
+// [{ age: 27, canVote: true },{ age: 2, canVote: false }]
+
+// let ans = arr.map((ele, ind) => {
+//   return { age: ele.age, canVote: ele.age > 18 };
+// });
+
+// console.log(ans);
