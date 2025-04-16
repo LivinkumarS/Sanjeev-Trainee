82473c29f46c8072224b7ee9fdc7fefc1bd78502
LivinkumarS <001liv100@ggmail.com>
2025-04-16 18:05:53 +0530

    DOM traversing, styling, changing contents, attributes, class names

--- app.js
+++ app.js
@@ -7,6 +7,47 @@
 
 // [h1]
 
-let h1 = document.getElementById("head");
+let h1 = document.querySelector("h1");
 
-console.log(h1);
+// console.log(h1.textContent);
+// h1.textContent = "I would love to code";
+
+// console.log(h1.innerHTML);
+// console.log(h1.textContent);
+
+// h1.innerHTML="<span>Hi Hello</span>"
+
+let a = document.querySelector("a");
+
+// console.log(a.getAttribute("href"));
+
+// a.setAttribute("id","anc")
+// a.setAttribute("href","facebook.in")
+
+// a.removeAttribute("href")
+
+// h1.style.backgroundColor="red"
+
+// console.log(h1.classList);
+
+// h1.classList.add("head3")
+// h1.classList.add("head4")
+
+// h1.classList.remove("head")
+
+// h1.classList.toggle("head9");
+
+// console.log(h1.parentElement);
+
+// let body = document.querySelector("body");
+
+// console.log(body.children);
+// console.log(body.firstElementChild);
+// console.log(body.lastElementChild);
+
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousElementSibling);
+
+
+
+console.log(document.querySelector("html").lastElementChild.firstElementChild.nextElementSibling);
