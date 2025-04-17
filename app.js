e6b1ae1c6ba00756c3f79c0f41babe56cf1d1270
LivinkumarS <001liv100@ggmail.com>
2025-04-17 17:54:07 +0530

    Event listeners

--- app.js
+++ app.js
@@ -1,61 +1,11 @@
-// let h1 = document.getElementById("head");
-// let h1 = document.getElementsByClassName("head");
-// let h1 = document.getElementsByTagName("h1");
+const h1 = document.querySelector("h1");
 
-// let h1 = document.querySelector("h1");
-// let h1 = document.querySelectorAll(".head");
+// document.querySelector("button").addEventListener("click", (event) => {
+//   console.log(event.target);
+//   h1.textContent = parseInt(h1.textContent) + 1;
+// });
 
-// [h1]
-
-let h1 = document.querySelector("h1");
-
-// console.log(h1.textContent);
-// h1.textContent = "I would love to code";
-
-// console.log(h1.innerHTML);
-// console.log(h1.textContent);
-
-// h1.innerHTML="<span>Hi Hello</span>"
-
-let a = document.querySelector("a");
-
-// console.log(a.getAttribute("href"));
-
-// a.setAttribute("id","anc")
-// a.setAttribute("href","facebook.in")
-
-// a.removeAttribute("href")
-
-// h1.style.backgroundColor="red"
-
-// console.log(h1.classList);
-
-// h1.classList.add("head3")
-// h1.classList.add("head4")
-
-// h1.classList.remove("head")
-
-// h1.classList.toggle("head9");
-
-// console.log(h1.parentElement);
-
-// let body = document.querySelector("body");
-
-// console.log(body.children);
-// console.log(body.firstElementChild);
-// console.log(body.lastElementChild);
-
-// console.log(h1.nextElementSibling);
-// console.log(h1.previousElementSibling);
-
-// console.log(document.querySelector("html").lastElementChild.firstElementChild.nextElementSibling);
-
-// let div = document.createElement("div");
-// div.textContent = "Hello Guys from div";
-// div.classList.add("div");
-
-// document.querySelector("body").appendChild(div);
-
-// document.querySelector("a").remove()
-
-// div.remove();
+function changeNum(event) {
+  console.log(event.target);
+  h1.textContent = parseInt(h1.textContent) + 1;
+}
