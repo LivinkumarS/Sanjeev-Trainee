f33813f5e358769d745357bb535b505bc48ff6cc
LivinkumarS <001liv100@ggmail.com>
2025-04-29 18:35:25 +0530

    State, Hook intro, useState

--- sanjeev training app/src/App.jsx
+++ sanjeev training app/src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Card from "./Card";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
+
+  const changeNum = () => {
+    setNumber((prev) => {
+      return prev + 1;
+    });
+  };
+
   return (
     <div>
-      <Card Name="Sanjeev" Role="Dev" />
-      <Card Name="Naveen" Role="Dev" />
-      <Card Name="Livin" Role="Dev" />
+      <Card Name="Sanjeev" Role="Dev" Number={number} />
+      <Card Name="Naveen" Role="Dev" Number={number} />
+      <Card Name="Livin" Role="Dev" Number={number} />
+      <button onClick={changeNum}>Clickme</button>
     </div>
   );
 }
--- sanjeev training app/src/Card.jsx
+++ sanjeev training app/src/Card.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Card(props) {
-
   return (
     <div>
       <h2>Name: {props.Name}</h2>
       <h2>Role: {props.Role}</h2>
+      <h1>{props.Number}</h1>
     </div>
   );
 }
