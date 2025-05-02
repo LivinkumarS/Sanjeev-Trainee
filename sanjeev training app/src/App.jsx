1980fb8edfd5da8da2e3322c2939f210704971a0
LivinkumarS <001liv100@ggmail.com>
2025-05-02 17:55:21 +0530

    Conditional Rendering

--- sanjeev training app/src/App.jsx
+++ sanjeev training app/src/App.jsx
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  const [name, setName] = useState("");
-
-  console.log(1);
   
-
-  // function printVal(e) {
-  //   // console.log(e.target.value); // the element
-  //   setName(e.target.value);
-  // }
 
   return (
     <div>
-      <input
-        type="text"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          console.log(name);
-        }}
-      >
-        Print Name
-      </button>
+      <h1>Conditional Rendering</h1>
+
+      {/* {value && <p>Hello Guys!</p>} */}
+      {value ? <p>Hello Guys!</p> : <p>Hi Guys!</p>}
     </div>
   );
 }
