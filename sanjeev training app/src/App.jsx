c57b2c9193c59ac930c2091da983dac2197e2e41
LivinkumarS <001liv100@ggmail.com>
2025-05-02 18:07:01 +0530

    ConditionalRendering Task

--- sanjeev training app/src/App.jsx
+++ sanjeev training app/src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  
+  const [value, setValue] = useState("false");
 
   return (
     <div>
       <h1>Conditional Rendering</h1>
 
-      {/* {value && <p>Hello Guys!</p>} */}
-      {value ? <p>Hello Guys!</p> : <p>Hi Guys!</p>}
+      {value ? <p>Hello</p> : <p>Hi</p>}
+
+      <button
+        onClick={() => {
+          setValue((prev) => {
+            return !prev;
+          });
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 }
